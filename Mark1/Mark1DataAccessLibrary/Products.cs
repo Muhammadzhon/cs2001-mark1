d62b27b06cccdc6070a9b85650756b1809207898
Mukhammadzhon <hmuhammadjon@gmail.com>
2020-12-04 19:01:11 +0600

    Revert "Revert "opened library class project with 2 ORM models (products and Customer)""
    This reverts commit 9e21dac738cda4b9d780812b091dede987d85a8b.

---
+++ Mark1/Mark1DataAccessLibrary/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mark1DataAccessLibrary
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Quantity { get; set; }
+
+        public int customerId { get; set; }
+        public Customer customer { get; set; }
+    }
+}
